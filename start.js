8d697cefdc3c9e04c218576903715676004d85f5
Michael <michaelkivo977@gmail.com>
2025-03-15 14:16:41 +0300

    Finish header for all pages

---
+++ start.js
@@ -0,0 +1,14 @@
+let link;
+if (window.location.pathname.split("/")[1] == 'index.html') {
+    link = document.getElementById("1");
+    link.style.setProperty("color", "#fff200");
+} else if (window.location.pathname.split("/")[1] == 'equipment.html') {
+    link = document.getElementById("2");
+    link.style.setProperty("color", "#fff200");
+} else if (window.location.pathname.split("/")[1] == 'makeorder.html') {
+    link = document.getElementById("3");
+    link.style.setProperty("color", "#fff200");
+} else if (window.location.pathname.split("/")[1] == 'account.html') {
+    link = document.getElementById("4");
+    link.style.setProperty("color", "#fff200");
+}
